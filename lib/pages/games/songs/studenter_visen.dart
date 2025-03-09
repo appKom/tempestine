5d5f946d0843ff9cdf59316d12213b86db128db6
Erlend Strøm <erl1ls@outlook.com>
2025-03-10 00:30:19 +0100

    Remove images from songs

--- lib/pages/games/songs/studenter_visen.dart
+++ lib/pages/games/songs/studenter_visen.dart
@@ -48,47 +48,32 @@ hva har vi å bestille?
     final padding = MediaQuery.of(context).padding + OnlineTheme.horizontalPadding;
 
     return Padding(
-      padding: EdgeInsets.only(top: padding.top, bottom: padding.bottom) + const EdgeInsets.symmetric(vertical: 64),
+      padding: padding + const EdgeInsets.symmetric(vertical: 64),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          SizedBox(
-            height: 267,
-            child: Image.asset(
-              'assets/images/studentervisen.png',
-              fit: BoxFit.cover,
+          const SizedBox(height: 24),
+          Text(
+            'Studenter Visen',
+            style: OnlineTheme.header(),
+          ),
+          const SizedBox(height: 24),
+          MarkdownBody(
+            data: _getText(),
+            styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
+              p: OnlineTheme.textStyle(),
+              h1: TextStyle(color: OnlineTheme.current.fg),
+              h2: TextStyle(color: OnlineTheme.current.fg),
+              h3: TextStyle(color: OnlineTheme.current.fg),
+              h4: TextStyle(color: OnlineTheme.current.fg),
+              h5: TextStyle(color: OnlineTheme.current.fg),
+              h6: TextStyle(color: OnlineTheme.current.fg),
+              horizontalRuleDecoration: BoxDecoration(
+                border: Border(top: BorderSide(width: 1, color: OnlineTheme.current.border)),
+              ),
             ),
           ),
-          Padding(
-            padding: EdgeInsets.only(left: padding.left, right: padding.right),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const SizedBox(height: 24),
-                Text(
-                  'Studenter Visen',
-                  style: OnlineTheme.header(),
-                ),
-                const SizedBox(height: 24),
-                MarkdownBody(
-                  data: _getText(),
-                  styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
-                    p: OnlineTheme.textStyle(),
-                    h1: TextStyle(color: OnlineTheme.current.fg),
-                    h2: TextStyle(color: OnlineTheme.current.fg),
-                    h3: TextStyle(color: OnlineTheme.current.fg),
-                    h4: TextStyle(color: OnlineTheme.current.fg),
-                    h5: TextStyle(color: OnlineTheme.current.fg),
-                    h6: TextStyle(color: OnlineTheme.current.fg),
-                    horizontalRuleDecoration: BoxDecoration(
-                      border: Border(top: BorderSide(width: 1, color: OnlineTheme.current.border)),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 24),
-              ],
-            ),
-          ),
+          const SizedBox(height: 24),
         ],
       ),
     );
--- lib/pages/games/songs/we_like_to_drink.dart
+++ lib/pages/games/songs/we_like_to_drink.dart
@@ -20,11 +20,6 @@ And when we drink with *NAVN* \n
 **3!** \n
 **2!** \n
 **1!** \n
----
-### **Hvis glasset ikke er tømt:**
-Why was he born so beautiful, why was he born at all? \n
-He's no fucking use to anyone, he's no fucking use at all! \n
-He's a cunt he's a cunt, he's a C U N T cunt!
 ''';
   }
 
@@ -33,47 +28,32 @@ He's a cunt he's a cunt, he's a C U N T cunt!
     final padding = MediaQuery.of(context).padding + OnlineTheme.horizontalPadding;
 
     return Padding(
-      padding: EdgeInsets.only(top: padding.top, bottom: padding.bottom) + const EdgeInsets.symmetric(vertical: 64),
+      padding: padding + const EdgeInsets.symmetric(vertical: 64),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          SizedBox(
-            height: 267,
-            child: Image.asset(
-              'assets/images/we_like_to_drink.png',
-              fit: BoxFit.cover,
+          const SizedBox(height: 24),
+          Text(
+            'We like to drink with',
+            style: OnlineTheme.header(),
+          ),
+          const SizedBox(height: 24),
+          MarkdownBody(
+            data: _getText(),
+            styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
+              p: OnlineTheme.textStyle(),
+              h1: TextStyle(color: OnlineTheme.current.fg),
+              h2: TextStyle(color: OnlineTheme.current.fg),
+              h3: TextStyle(color: OnlineTheme.current.fg),
+              h4: TextStyle(color: OnlineTheme.current.fg),
+              h5: TextStyle(color: OnlineTheme.current.fg),
+              h6: TextStyle(color: OnlineTheme.current.fg),
+              horizontalRuleDecoration: BoxDecoration(
+                border: Border(top: BorderSide(width: 1, color: OnlineTheme.current.border)),
+              ),
             ),
           ),
-          Padding(
-            padding: EdgeInsets.only(left: padding.left, right: padding.right),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const SizedBox(height: 24),
-                Text(
-                  'We like to drink with',
-                  style: OnlineTheme.header(),
-                ),
-                const SizedBox(height: 24),
-                MarkdownBody(
-                  data: _getText(),
-                  styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
-                    p: OnlineTheme.textStyle(),
-                    h1: TextStyle(color: OnlineTheme.current.fg),
-                    h2: TextStyle(color: OnlineTheme.current.fg),
-                    h3: TextStyle(color: OnlineTheme.current.fg),
-                    h4: TextStyle(color: OnlineTheme.current.fg),
-                    h5: TextStyle(color: OnlineTheme.current.fg),
-                    h6: TextStyle(color: OnlineTheme.current.fg),
-                    horizontalRuleDecoration: BoxDecoration(
-                      border: Border(top: BorderSide(width: 1, color: OnlineTheme.current.border)),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 24),
-              ],
-            ),
-          ),
+          const SizedBox(height: 24),
         ],
       ),
     );
